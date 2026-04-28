42342cdf952643c84af3c05210a29d630a363667
ahmed <ahmedhambuta9@gmail.com>
2026-04-28 19:13:03 +0300

    EDIT: now returns the category name not the ID of the category

--- OpenFashion/products/serializers.py
+++ OpenFashion/products/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    
+    category = serializers.CharField(source='category.name', read_only=True)
+
     class Meta:
         model = Product
         fields = ['id', 'name', 'price', 'category', 'description', 'product_image']
