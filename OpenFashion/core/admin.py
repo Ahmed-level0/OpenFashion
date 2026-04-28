8825e62a7874f8469945ea876264e7c957cb91fb
ahmed <ahmedhambuta9@gmail.com>
2026-04-28 19:24:05 +0300

    EDIT: edited the admin site display

--- OpenFashion/core/admin.py
+++ OpenFashion/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 
 # Register your models here.
+
+AdminSite.site_header = 'OpenFashion Admin'
+AdminSite.site_title = 'OpenFashion Admin'
+AdminSite.index_title = 'Welcome to OpenFashion Admin'
